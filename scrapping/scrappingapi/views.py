b047775e172b3df48ae7e97ada9bbf85b4c96293
sameerajmera <sameer4jain@gmail.com>
2020-10-25 23:04:24 +0530

    changes in db

--- scrapping/scrappingapi/views.py
+++ scrapping/scrappingapi/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from .scrap import scrapFunct
 from django.http import HttpResponse
 from rest_framework.views import APIView 
 import pandas as pd
+import json
 # Create your views here.
 
 class scrapAPI(APIView):
@@ -11,4 +13,4 @@ class scrapAPI(APIView):
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename=filename.csv'
         results.to_csv(path_or_buf=response,sep=',',float_format='%.2f',index=False,decimal=".")
-        return response
+        return (response)
